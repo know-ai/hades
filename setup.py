f83b6ec15a163ffbdd77abe1db27ea4049b83694
Carlos Rivero <cdrr.rivero@gmail.com>
2022-04-09 17:30:34 -0400

    Only test for python version >=3.6

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=_requirements,
     classifiers=[
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
