0f4fd9cf200a0ccab6610dac42ef10d6e7b0f6ae
Carlos Rivero <cdrr.rivero@gmail.com>
2022-06-03 09:16:18 -0400

    Adding AlarmStates and Priorities Tables

--- pyhades/dbmodels/alarms.py
+++ pyhades/dbmodels/alarms.py
@@ -252,6 +252,247 @@ class AlarmsDB(BaseModel):
         }
 
 
+class AlarmStates(BaseModel):
+    r"""
+    Based on ISA 18.2
+    """
+
+    name = CharField(unique=True)
+    mnemonic = CharField(max_length=20)
+    desc = CharField()
+
+    @classmethod
+    def create(cls, name:str, mnemonic:str, desc:str)-> dict:
+        r"""
+        You can use Model.create() to create a new model instance. This method accepts keyword arguments, where the keys correspond 
+        to the names of the model's fields. A new instance is returned and a row is added to the table.
+
+        ```python
+        >>> AlarmsType.create(name='Unacknowledged', mnemonic='UNACKED', desc='Alarm unacknowledged')
+        {
+            'message': (str)
+            'data': (dict) {
+                'id': 1,
+                'name': 'unacknowledged',
+                'mnemonic': 'UNACKED',
+                'desc': 'Alarm unacknowledged'
+            }
+        }
+        ```
+        
+        This will INSERT a new row into the database. The primary key will automatically be retrieved and stored on the model instance.
+
+        **Parameters**
+
+        * **name:** (str), Industrial protocol name
+
+        **Returns**
+
+        * **result:** (dict) --> {'message': (str), 'data': (dict) row serialized}
+
+        """
+        result = dict()
+        data = dict()
+        name = name.lower()
+
+        if not cls.name_exist(name):
+
+            query = cls(name=name, mnemonic=mnemonic, desc=desc)
+            query.save()
+            
+            message = f"Alarm state {name} created successfully"
+            data.update(query.serialize())
+
+            result.update(
+                {
+                    'message': message, 
+                    'data': data
+                }
+            )
+            return result
+
+        message = f"Alarm state {name} is already into database"
+        result.update(
+            {
+                'message': message, 
+                'data': data
+            }
+        )
+        return result
+
+    @classmethod
+    def read_by_name(cls, name:str)->bool:
+        r"""
+        Get instance by its a name
+
+        **Parameters**
+
+        * **name:** (str) Alarm type name
+
+        **Returns**
+
+        * **bool:** If True, name exist into database 
+        """
+        query = cls.get_or_none(name=name)
+        
+        if query is not None:
+
+            return query.serialize()
+        
+        return None
+
+    @classmethod
+    def name_exist(cls, name:str)->bool:
+        r"""
+        Verify is a name exist into database
+
+        **Parameters**
+
+        * **name:** (str) Alarm type name
+
+        **Returns**
+
+        * **bool:** If True, name exist into database 
+        """
+        query = cls.get_or_none(name=name)
+        
+        if query is not None:
+
+            return True
+        
+        return False
+
+    def serialize(self)-> dict:
+        r"""
+        Serialize database record to a jsonable object
+        """
+
+        return {
+            "id": self.id,
+            "name": self.name,
+            "mnemonic": self.mnemonic,
+            "desc": self.desc
+        }
+
+
+class AlarmsPriorities(BaseModel):
+    r"""
+    Based on ISA 18.2
+    """
+
+    priority = IntegerField(unique=True)
+    desc = CharField()
+
+    @classmethod
+    def create(cls, priority:int, desc:str)-> dict:
+        r"""
+        You can use Model.create() to create a new model instance. This method accepts keyword arguments, where the keys correspond 
+        to the names of the model's fields. A new instance is returned and a row is added to the table.
+
+        ```python
+        >>> AlarmsType.create(priority=1, desc='Low priority')
+        {
+            'message': (str)
+            'data': (dict) {
+                'id': 1,
+                'priority': 1,
+                'desc': 'Low priority'
+            }
+        }
+        ```
+        
+        This will INSERT a new row into the database. The primary key will automatically be retrieved and stored on the model instance.
+
+        **Parameters**
+
+        * **name:** (str), Industrial protocol name
+
+        **Returns**
+
+        * **result:** (dict) --> {'message': (str), 'data': (dict) row serialized}
+
+        """
+        result = dict()
+        data = dict()
+
+        if not cls.priority_exist(priority):
+
+            query = cls(priority=priority, desc=desc)
+            query.save()
+            
+            message = f"Alarm priority {priority} created successfully"
+            data.update(query.serialize())
+
+            result.update(
+                {
+                    'message': message, 
+                    'data': data
+                }
+            )
+            return result
+
+        message = f"Alarm priority {priority} is already into database"
+        result.update(
+            {
+                'message': message, 
+                'data': data
+            }
+        )
+        return result
+
+    @classmethod
+    def read_by_priority(cls, priority:int)->dict:
+        r"""
+        Get instance by its priority
+
+        **Parameters**
+
+        * **priority:** (str) Alarm priority
+
+        **Returns**
+
+        * **bool:** If True, priority exist into database 
+        """
+        query = cls.get_or_none(priority=priority)
+        
+        if query is not None:
+
+            return query.serialize()
+        
+        return None
+
+    @classmethod
+    def priority_exist(cls, priority:int)->bool:
+        r"""
+        Verify is a priority exist into database
+
+        **Parameters**
+
+        * **priority:** (str) Alarm priority
+
+        **Returns**
+
+        * **bool:** If True, priority exist into database 
+        """
+        query = cls.get_or_none(priority=priority)
+        
+        if query is not None:
+
+            return True
+        
+        return False
+
+    def serialize(self)-> dict:
+        r"""
+        Serialize database record to a jsonable object
+        """
+
+        return {
+            "id": self.id,
+            "priority": self.priority,
+            "desc": self.desc
+        }
+
 
 class AlarmsLogging(BaseModel):
     
